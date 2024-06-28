1890c6f22733e6bce49ef37d095ccf0ed756006f
Sahad mp <muhdsahad4916@gmail.com>
2024-06-28 21:42:57 +0530

    setuping clean architeture and responsive folder stercture

---
+++ lib/application/features/auth/signIn/ui/sign_in.dart
@@ -0,0 +1,24 @@
+// import 'package:chat_app_ai/application/core/responsive/responsive_main.dart';
+// import 'package:flutter/material.dart';
+
+
+// class SignIn extends StatefulWidget {
+//   final String directiontext;
+
+//   const SignIn({super.key, required this.directiontext});
+
+//   @override
+//   State<SignIn> createState() => _SignInState();
+// }
+
+// class _SignInState extends State<SignIn> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return  Scaffold(
+//       body: ResponsiveLayOut(
+//           mobileScaffold: SignMobLayOut(directiontext: widget.directiontext),
+//           tabletScaffold: SignTebLayOut(directiontext: widget.directiontext),
+//           desktopScaffold: SignDeskLayOut(directiontext: widget.directiontext)),
+//     );
+//   }
+// }
